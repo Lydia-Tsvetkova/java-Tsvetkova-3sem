3774a1c370db8ae9947bd80f69f919eb35a6f30d
Lydia-Tsvetkova <90324396+Lydia-Tsvetkova@users.noreply.github.com>
2022-05-09 20:23:45 +0300

    New changes

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module ru.spbu.arts.javafx {
+    requires javafx.base; // другие модули, которые используем
+    requires javafx.graphics;
+    requires javafx.controls;
+
+    //разрешаем библиотеке javafx исследовать содержимое нашего пакета с кодом
+    opens ru.spbu.arts.java.messenger;
+    opens ru.spbu.arts.java.button;
+    opens ru.spbu.arts.javafx;
+
+}
